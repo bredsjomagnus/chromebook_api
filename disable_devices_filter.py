7e27c21aea9604bf449e8109ef5320955081714f
Magnus Andersson <bredsjomagnus@yahoo.se>
2022-02-03 08:30:18 +0100

    cleaning up

--- disable_devices_filter.py
+++ disable_devices_filter.py
@@ -1,5 +1,3 @@
 disable_devices_filter = {
-    # "orgUnitPath": ["7A", "7B", "7C", "8A", "8B", "8C", "8D", "9A", "9B", "9C"],
-     "annotatedAssetId": ["Edubook150", "Edubook325", "Edubook287", "Edubook296", "Edubook008", "Edubook293"]
-    # "annotatedAssetId": ["Edubook273"]
+    "annotatedAssetId": ["Edubook150", "Edubook325", "Edubook287", "Edubook296", "Edubook008", "Edubook293"]
 }
