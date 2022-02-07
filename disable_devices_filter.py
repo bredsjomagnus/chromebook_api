ca2f2e2d880d67a9dc78db1787dd96b84f0e4b6d
Magnus Andersson <bredsjomagnus@yahoo.se>
2022-02-07 11:03:01 +0100

    Added 572

--- disable_devices_filter.py
+++ disable_devices_filter.py
@@ -1,3 +1,3 @@
 disable_devices_filter = {
-    "annotatedAssetId": ["Edubook150", "Edubook325", "Edubook287", "Edubook296", "Edubook008", "Edubook293"]
+    "annotatedAssetId": ["Edubook150", "Edubook325", "Edubook287", "Edubook296", "Edubook008", "Edubook293", "Edubook572"]
 }
