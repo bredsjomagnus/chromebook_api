351a0e5d3995481c4638a40908db885f91b559a4
Magnus Andersson <bredsjomagnus@yahoo.se>
2022-02-09 15:08:22 +0100

    comment to not FJs unit

--- disable_devices_filter.py
+++ disable_devices_filter.py
@@ -4,3 +4,4 @@ disable_devices_filter = {
 # disable_devices_filter = {
 #     "annotatedAssetId": ["Edubook150", "Edubook325", "Edubook287", "Edubook296", "Edubook008", "Edubook293", "Edubook572", "Edubook390"]
 # }
+# Edubook287 Faezas enhet
