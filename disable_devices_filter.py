d197c6588df306e05227307c53e038e2bb54ecc9
Magnus Andersson <bredsjomagnus@yahoo.se>
2022-02-17 10:44:26 +0100

    set the exceptions for next week

--- disable_devices_filter.py
+++ disable_devices_filter.py
@@ -1,7 +1,9 @@
 disable_devices_filter = {
-    "annotatedAssetId": ["Edubook150", "Edubook325", "Edubook296", "Edubook008", "Edubook293", "Edubook572", "Edubook390", "Edubook393"]
+    "annotatedAssetId": ["Edubook150", "Edubook325", "Edubook296", "Edubook293", "Edubook572", "Edubook390", "Edubook393"]
 }
 # disable_devices_filter = {
 #     "annotatedAssetId": ["Edubook150", "Edubook325", "Edubook287", "Edubook296", "Edubook008", "Edubook293", "Edubook572", "Edubook390"]
 # }
 # Edubook287 Faezas enhet
+# Edubook018 Lidyas enhet
+# Edubook008 Yasmins enhet
