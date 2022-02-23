6d65d4903512374c6abf7d000d33bfd5420acb50
Magnus Andersson <bredsjomagnus@yahoo.se>
2022-02-23 21:08:59 +0100

    updated filter

--- disable_devices_filter.py
+++ disable_devices_filter.py
@@ -1,5 +1,5 @@
 disable_devices_filter = {
     # "orgUnitPath": ["7A", "7B", "7C", "8A", "8B", "8C", "8D", "9A", "9B", "9C"],
     # "annotatedAssetId": ["Edubook273"]
-    "annotatedAssetId": ["Edubook150", "Edubook325", "Edubook287", "Edubook296", "Edubook008"]
+    "annotatedAssetId": ["Edubook018", "Edubook291", "Edubook150", "Edubook325", "Edubook287", "Edubook296", "Edubook008"]
 }
