7c4a18d4c3669d7b98ad6bf4628dd1b2480727fe
Magnus Andersson <bredsjomagnus@yahoo.se>
2022-03-02 08:17:01 +0100

    lade till 004

--- disable_devices_filter.py
+++ disable_devices_filter.py
@@ -1,5 +1,5 @@
 disable_devices_filter = {
-    "annotatedAssetId": ["Edubook008", "Edubook150", "Edubook325", "Edubook296", "Edubook293", "Edubook572", "Edubook390", "Edubook393"]
+    "annotatedAssetId": ["Edubook004", "Edubook008", "Edubook150", "Edubook325", "Edubook296", "Edubook293", "Edubook572", "Edubook390", "Edubook393"]
 }
 # disable_devices_filter = {
 #     "annotatedAssetId": ["Edubook150", "Edubook325", "Edubook287", "Edubook296", "Edubook008", "Edubook293", "Edubook572", "Edubook390"]
@@ -7,3 +7,4 @@ disable_devices_filter = {
 # Edubook287 Faezas enhet
 # Edubook018 Lidyas enhet
 # Edubook008 Yasmins enhet
+# Edubook004 Sundus enhet
