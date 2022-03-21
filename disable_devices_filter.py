0f9b58a77023edb231b8e0148351c4cc93a3c78b
Magnus Andersson <bredsjomagnus@yahoo.se>
2022-03-21 09:19:29 +0100

    removed 287

--- disable_devices_filter.py
+++ disable_devices_filter.py
@@ -1,5 +1,5 @@
 disable_devices_filter = {
-    "annotatedAssetId": ["Edubook311", "Edubook004", "Edubook008", "Edubook150", "Edubook325", "Edubook296", "Edubook293", "Edubook572", "Edubook390", "Edubook393", "Edubook287"]
+    "annotatedAssetId": ["Edubook311", "Edubook004", "Edubook008", "Edubook150", "Edubook325", "Edubook296", "Edubook293", "Edubook572", "Edubook390", "Edubook393"]
 }
 # disable_devices_filter = {
 #     "annotatedAssetId": ["Edubook150", "Edubook325", "Edubook287", "Edubook296", "Edubook008", "Edubook293", "Edubook572", "Edubook390"]
